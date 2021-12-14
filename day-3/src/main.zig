6448f1ca3fe8f778f45ff85ee65f810b3059f320
GigaGrunch <gigagrunch@gmail.com>
2021-12-14 16:37:01 +0100

    parse all samples in advance

--- day-3/src/main.zig
+++ day-3/src/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const use_test_input = false;
+const use_test_input = true;
 const filename = if (use_test_input) "test_input" else "real_input";
 const sample_count = if (use_test_input) 12 else 1000;
 const sample_length = if (use_test_input) 5 else 12;
@@ -10,30 +10,45 @@ pub fn main() !void {
     const file = try cwd.openFile(filename, .{});
     defer file.close();
 
-    var buffer: [sample_length]u8 = undefined;
+    var file_text: [sample_count * (sample_length + 1) + 1]u8 = undefined;
+    _ = try file.readAll(file_text[0..]);
 
-    var one_counts = [_]u32{0} ** sample_length;
+    var samples: [sample_count][]const u8 = undefined;
     var sample_index: usize = 0;
     while (sample_index < sample_count):(sample_index += 1) {
-        const line = try file.reader().readUntilDelimiter(buffer[0..], '\n');
+        const start = sample_index * (sample_length + 1);
+        const end = start + sample_length;
+        samples[sample_index] = file_text[start..end];
+    }
+
+    for (samples) |sample| {
+        std.debug.print("{s} ", .{ sample });
+    }
+
+    std.debug.print("\n", .{});
+
+    // var one_counts = [_]u32{0} ** sample_length;
+    // var sample_index: usize = 0;
+    // while (sample_index < sample_count):(sample_index += 1) {
+    //     const line = try file.reader().readUntilDelimiter(buffer[0..], '\n');
         
-        for (line) |char, i| {
-            if (char == '1') {
-                one_counts[i] += 1;
-            }
-        }
-    }
+    //     for (line) |char, i| {
+    //         if (char == '1') {
+    //             one_counts[i] += 1;
+    //         }
+    //     }
+    // }
 
-    var gamma_string: [sample_length]u8 = undefined;
-    var epsilon_string: [sample_length]u8 = undefined;
-    for (one_counts) |count, i| {
-        gamma_string[i] = if (count > sample_count / 2) '1' else '0';
-        epsilon_string[i] = if (count > sample_count / 2) '0' else '1';
-    }
+    // var gamma_string: [sample_length]u8 = undefined;
+    // var epsilon_string: [sample_length]u8 = undefined;
+    // for (one_counts) |count, i| {
+    //     gamma_string[i] = if (count > sample_count / 2) '1' else '0';
+    //     epsilon_string[i] = if (count > sample_count / 2) '0' else '1';
+    // }
 
-    const gamma = try std.fmt.parseInt(u32, gamma_string[0..], 2);
-    const epsilon = try std.fmt.parseInt(u32, epsilon_string[0..], 2);
-    const power_consumption = gamma * epsilon;
+    // const gamma = try std.fmt.parseInt(u32, gamma_string[0..], 2);
+    // const epsilon = try std.fmt.parseInt(u32, epsilon_string[0..], 2);
+    // const power_consumption = gamma * epsilon;
 
-    std.debug.print("power consumption is {d}\n", .{ power_consumption });
+    // std.debug.print("power consumption is {d}\n", .{ power_consumption });
 }
