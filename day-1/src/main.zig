4eba6d0e8fe2f2697da71b6f81d899bece2cd0d4
GigaGrunch <gigagrunch@gmail.com>
2021-12-14 16:37:01 +0100

    calculate depths windows and count increments

--- day-1/src/main.zig
+++ day-1/src/main.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
-var read_buffer_array: [100]u8 = undefined;
-var read_buffer = read_buffer_array[0..];
+var i: usize = undefined;
 
 pub fn main() !void {
     var cwd = std.fs.cwd();
@@ -9,17 +8,32 @@ pub fn main() !void {
     defer input_file.close();
     
     var depths: [2000]u32 = undefined;
-    var i: u32 = 0;
+    i = 0;
     while (i < 2000):(i += 1) {
         depths[i] = try getNextInt(input_file);
-        std.debug.print("{d} ", .{ depths[i] });
     }
 
     var increase_count: u32 = 0;
+
+    var previous = getDepthSum(depths[0..3]);
+    i = 1;
+    while (i < 1998):(i += 1) {
+        const depth = getDepthSum(depths[i..i+3]);
+        if (depth > previous) {
+            increase_count += 1;
+        }
+        previous = depth;
+    }
+
     std.debug.print("increase count: {d}\n", .{ increase_count });
 }
 
 fn getNextInt(file: std.fs.File) !u32 {
-    const line = try file.reader().readUntilDelimiter(read_buffer, '\n');
+    var read_buffer: [100]u8 = undefined;
+    const line = try file.reader().readUntilDelimiter(read_buffer[0..], '\n');
     return try std.fmt.parseInt(u32, line, 10);
+}
+
+fn getDepthSum(window: []u32) u32 {
+    return window[0] + window[1] + window[2];
 }
