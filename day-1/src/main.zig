07d37f8e42ba45da4cf42eedffca0b6a1ed35e57
GigaGrunch <gigagrunch@gmail.com>
2021-12-14 16:37:01 +0100

    assume total count of 2000 and parse all depths at once

--- day-1/src/main.zig
+++ day-1/src/main.zig
@@ -7,21 +7,19 @@ pub fn main() !void {
     var cwd = std.fs.cwd();
     var input_file = try cwd.openFile("input", .{});
     defer input_file.close();
-
-    var increase_count: u32 = 0;
-    var previous = (try getNextInt(input_file)) orelse unreachable;
-    while (try getNextInt(input_file)) |depth| {
-        if (depth > previous) increase_count += 1;
-        previous = depth;
+    
+    var depths: [2000]u32 = undefined;
+    var i: u32 = 0;
+    while (i < 2000):(i += 1) {
+        depths[i] = try getNextInt(input_file);
+        std.debug.print("{d} ", .{ depths[i] });
     }
 
+    var increase_count: u32 = 0;
     std.debug.print("increase count: {d}\n", .{ increase_count });
 }
 
-fn getNextInt(file: std.fs.File) !?u32 {
-    if (try file.reader().readUntilDelimiterOrEof(read_buffer, '\n')) |line| {
-        return try std.fmt.parseInt(u32, line, 10);
-    } else {
-        return null;
-    }
+fn getNextInt(file: std.fs.File) !u32 {
+    const line = try file.reader().readUntilDelimiter(read_buffer, '\n');
+    return try std.fmt.parseInt(u32, line, 10);
 }
