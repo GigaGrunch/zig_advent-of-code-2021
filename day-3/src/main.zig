fb3356a49c9619e72f749f81c75a25ea7cd5f71f
GigaGrunch <gigagrunch@gmail.com>
2021-12-14 16:37:01 +0100

    find oxygen index

--- day-3/src/main.zig
+++ day-3/src/main.zig
@@ -13,13 +13,7 @@ pub fn main() !void {
     var file_text: [sample_count * (sample_length + 1) + 1]u8 = undefined;
     _ = try file.readAll(file_text[0..]);
 
-    var samples: [sample_count][]const u8 = undefined;
-    var sample_index: usize = 0;
-    while (sample_index < sample_count):(sample_index += 1) {
-        const start = sample_index * (sample_length + 1);
-        const end = start + sample_length;
-        samples[sample_index] = file_text[start..end];
-    }
+    const samples = getSamples(file_text[0..]);
 
     var one_counts = [_]u32{0} ** sample_length;
     for (samples) |sample| {
@@ -42,4 +36,40 @@ pub fn main() !void {
     const power_consumption = gamma * epsilon;
 
     std.debug.print("power consumption is {d}\n", .{ power_consumption });
+
+    var oxygen_candidates = [_]bool{true} ** sample_count;
+    var oxygen_candidates_count: usize = sample_count;
+    for (one_counts) |count, digit_index| {
+        const needs_one = count > sample_count / 2;
+
+        for (samples) |sample, sample_index| {
+            if (! oxygen_candidates[sample_index]) continue;
+
+            if (needs_one != (sample[digit_index] == '1')) {
+                oxygen_candidates[sample_index] = false;
+                oxygen_candidates_count -= 1;
+            }
+        }
+
+        if (oxygen_candidates_count <= 1) break;
+    }
+
+    const oxygen_index = 
+    for (oxygen_candidates) |is_condidate, i| {
+        if (is_condidate) break i;
+    } else unreachable;
+
+    std.debug.print("oxygen index: {d}\n", .{ oxygen_index });
 }
+
+fn getSamples(file_text: []const u8) [sample_count][]const u8 {
+    var samples: [sample_count][]const u8 = undefined;
+    var sample_index: usize = 0;
+    while (sample_index < sample_count):(sample_index += 1) {
+        const start = sample_index * (sample_length + 1);
+        const end = start + sample_length;
+        samples[sample_index] = file_text[start..end];
+    }
+    return samples;
+}
+
