22a68099fd9755ed1c636ed7e8a9a9424693d33d
GigaGrunch <gigagrunch@gmail.com>
2021-12-14 16:37:01 +0100

    parse all commands

--- day-2/src/main.zig
+++ day-2/src/main.zig
@@ -1,9 +1,42 @@
 const std = @import("std");
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+var i: usize = undefined;
+const input_length = 1000;
+
+pub fn main() !void {
+    var cwd = std.fs.cwd();
+    var input_file = try cwd.openFile("input", .{});
+    defer input_file.close();
+
+    i = 0;
+    while (i < input_length):(i += 1) {
+        const command = try getNextCommand(input_file);
+        std.debug.print("{} {} ", .{ command.type, command.value });
+    }
 }
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+fn getNextCommand(file: std.fs.File) !Command {
+    var buffer: [100]u8 = undefined;
+    const line = try file.reader().readUntilDelimiter(buffer[0..], '\n');
+    var split_it = std.mem.split(u8, line, " ");
+    const type_string = split_it.next() orelse unreachable;
+    const value_string = split_it.next() orelse unreachable;
+    const value = try std.fmt.parseInt(u32, value_string, 10);
+
+    var command = Command { .value = value };
+
+    if (std.mem.eql(u8, type_string, "forward")) {
+        command.type = .Forward;
+    } else if (std.mem.eql(u8, type_string, "down")) {
+        command.type = .Down;
+    } else if (std.mem.eql(u8, type_string, "up")) {
+        command.type = .Up;
+    } else unreachable;
+
+    return command;
 }
+
+const Command = struct {
+    type: enum { Forward, Down, Up } = undefined,
+    value: u32,
+};
