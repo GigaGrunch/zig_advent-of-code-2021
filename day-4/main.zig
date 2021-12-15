3776bdc250b6cb1d1d6e2c3d8868e2e80bf62378
GigaGrunch <gigagrunch@gmail.com>
2021-12-15 09:33:25 +0100

    get winning board and score

--- day-4/main.zig
+++ day-4/main.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
 
-const use_test_input = true;
+const use_test_input = false;
 const filename = if (use_test_input) "example_input" else "real_input";
 const draw_count = if (use_test_input) 27 else 100;
+const board_count = if (use_test_input) 3 else 100;
 
 pub fn main() !void {
     std.debug.print("--- Day 4 ---\n", .{});
@@ -25,121 +26,147 @@ pub fn main() !void {
         std.debug.print("\n", .{});
     }
 
-    std.debug.print("\n", .{});
+    var first_board_index: usize = 127;
+    var first_board_turn: u7 = 127;
+    var first_board_score: u32 = 0;
 
-    var board: [25]u7 = undefined;
-    var draw_turns = [_]?u7 {null} ** 25;
-    {
-        std.debug.print("Board {:>2}: ", .{ 0 });
+    var board_index: usize = 0;
+    while (board_index < board_count):(board_index += 1) {
+        std.debug.print("\n", .{});
 
-        try file.reader().skipUntilDelimiterOrEof('\n');
-        var buffer: [3]u8 = undefined;
-        const whitespace = [_]u8 { ' ', '\n' };
+        var board: [25]u7 = undefined;
+        var draw_turns = [_]?u7 {null} ** 25;
+        {
+            std.debug.print("Board {:>2}: ", .{ 0 });
 
-        var row: usize = 0;
-        while (row < 5):(row += 1) {
-            var column: usize = 0;
-            while (column < 5):(column += 1) {
-                const i = row * 5 + column;
+            try file.reader().skipUntilDelimiterOrEof('\n');
+            var buffer: [3]u8 = undefined;
+            const whitespace = [_]u8 { ' ', '\n' };
 
-                _ = try file.reader().read(buffer[0..]);
-                const num_string = std.mem.trim(u8, buffer[0..], whitespace[0..]);
-                board[i] = try std.fmt.parseInt(u7, num_string, 10);
+            var row: usize = 0;
+            while (row < 5):(row += 1) {
+                var column: usize = 0;
+                while (column < 5):(column += 1) {
+                    const i = row * 5 + column;
 
-                for (draw_numbers) |draw, turn| {
-                    if (draw == board[i]) {
-                        draw_turns[i] = @intCast(u7, turn);
+                    _ = try file.reader().read(buffer[0..]);
+                    const num_string = std.mem.trim(u8, buffer[0..], whitespace[0..]);
+                    board[i] = try std.fmt.parseInt(u7, num_string, 10);
+
+                    for (draw_numbers) |draw, turn| {
+                        if (draw == board[i]) {
+                            draw_turns[i] = @intCast(u7, turn);
+                            break;
+                        }
+                    }
+
+                    std.debug.print("{:>2}", .{ board[i] });
+                    var turn_string_buffer: [5]u8 = undefined;
+                    var turn_string: []u8 = undefined;
+                    if (draw_turns[i]) |turn| {
+                        turn_string = try std.fmt.bufPrint(turn_string_buffer[0..], "({})", .{ turn });
+                    } else {
+                        turn_string = try std.fmt.bufPrint(turn_string_buffer[0..], "(-)", .{});
+                    }
+                    std.debug.print("{s:<5} ", .{ turn_string });
+                }
+                std.debug.print("\n          ", .{});
+            }
+        }
+
+        std.debug.print("\n", .{});
+
+        var win_turn: u7 = 127;
+
+        var o_winning_row: ?usize = null;
+        {
+            var row: usize = 0;
+            while (row < 5):(row += 1) {
+                var o_row_win_turn: ?u7 = null;
+
+                var column: usize = 0;
+                while (column < 5):(column += 1) {
+                    const i = row * 5 + column;
+
+                    if (draw_turns[i]) |turn| {
+                        if (o_row_win_turn) |row_win_turn| {
+                            o_row_win_turn = @maximum(turn, row_win_turn);
+                        } else {
+                            o_row_win_turn = turn;
+                        }
+                    } else {
+                        o_row_win_turn = null;
                         break;
                     }
                 }
 
-                std.debug.print("{:>2}", .{ board[i] });
-                var turn_string_buffer: [5]u8 = undefined;
-                var turn_string: []u8 = undefined;
-                if (draw_turns[i]) |turn| {
-                    turn_string = try std.fmt.bufPrint(turn_string_buffer[0..], "({})", .{ turn });
-                } else {
-                    turn_string = try std.fmt.bufPrint(turn_string_buffer[0..], "(-)", .{});
+                if (o_row_win_turn) |row_win_turn| {
+                    if (row_win_turn < win_turn) {
+                        win_turn = row_win_turn;
+                        o_winning_row = row;
+                    }
                 }
-                std.debug.print("{s:<5} ", .{ turn_string });
             }
-            std.debug.print("\n          ", .{});
         }
-    }
 
-    std.debug.print("\n", .{});
-
-    var win_turn: u7 = 127;
-
-    var o_winning_row: ?usize = null;
-    {
-        var row: usize = 0;
-        while (row < 5):(row += 1) {
-            var o_row_win_turn: ?u7 = null;
-
+        var o_winning_column: ?usize = null;
+        {
             var column: usize = 0;
             while (column < 5):(column += 1) {
-                const i = row * 5 + column;
+                var o_column_win_turn: ?u7 = null;
 
-                if (draw_turns[i]) |turn| {
-                    if (o_row_win_turn) |row_win_turn| {
-                        o_row_win_turn = @maximum(turn, row_win_turn);
+                var row: usize = 0;
+                while (row < 5):(row += 1) {
+                    const i = row * 5 + column;
+
+                    if (draw_turns[i]) |turn| {
+                        if (o_column_win_turn) |column_win_turn| {
+                            o_column_win_turn = @maximum(turn, column_win_turn);
+                        } else {
+                            o_column_win_turn = turn;
+                        }
                     } else {
-                        o_row_win_turn = turn;
+                        o_column_win_turn = null;
+                        break;
                     }
-                } else {
-                    o_row_win_turn = null;
-                    break;
                 }
-            }
 
-            if (o_row_win_turn) |row_win_turn| {
-                if (row_win_turn < win_turn) {
-                    win_turn = row_win_turn;
-                    o_winning_row = row;
+                if (o_column_win_turn) |column_win_turn| {
+                    if (column_win_turn < win_turn) {
+                        win_turn = column_win_turn;
+                        o_winning_row = null;
+                        o_winning_column = column;
+                    }
                 }
             }
         }
-    }
 
-    var o_winning_column: ?usize = null;
-    {
-        var column: usize = 0;
-        while (column < 5):(column += 1) {
-            var o_column_win_turn: ?u7 = null;
+        if (o_winning_row) |winning_row| {
+            std.debug.print("winning row: {}({})\n", .{ winning_row, win_turn });
+        }
+        if (o_winning_column) |winning_column| {
+            std.debug.print("winning column: {}({})\n", .{ winning_column, win_turn });
+        }
 
-            var row: usize = 0;
-            while (row < 5):(row += 1) {
-                const i = row * 5 + column;
-
-                if (draw_turns[i]) |turn| {
-                    if (o_column_win_turn) |column_win_turn| {
-                        o_column_win_turn = @maximum(turn, column_win_turn);
-                    } else {
-                        o_column_win_turn = turn;
-                    }
-                } else {
-                    o_column_win_turn = null;
-                    break;
-                }
+        var score: u32 = 0;
+        for (board) |num, i| {
+            if (draw_turns[i] == null or draw_turns[i].? > win_turn) {
+                score += num;
             }
+        }
+        score *= draw_numbers[win_turn];
+        std.debug.print("score: {}\n", .{ score });
 
-            if (o_column_win_turn) |column_win_turn| {
-                if (column_win_turn < win_turn) {
-                    win_turn = column_win_turn;
-                    o_winning_row = null;
-                    o_winning_column = column;
-                }
-            }
+        if (win_turn < first_board_turn) {
+            first_board_turn = win_turn;
+            first_board_score = score;
+            first_board_index = board_index;
         }
     }
 
-    if (o_winning_row) |winning_row| {
-        std.debug.print("winning row: {}({})\n", .{ winning_row, win_turn });
-    }
-    if (o_winning_column) |winning_column| {
-        std.debug.print("winning column: {}({})\n", .{ winning_column, win_turn });
-    }
+    std.debug.print(
+        "\nboard {} wins at turn {} with score of {}\n",
+        .{ first_board_index, first_board_turn, first_board_score });
 }
 
 fn drawNext(file: std.fs.File, delimiter: u8) !u7 {
