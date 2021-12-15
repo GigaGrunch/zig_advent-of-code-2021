3a87c6f11e5fcdc7b0ba3c806bb1dbe3416c99ea
GigaGrunch <gigagrunch@gmail.com>
2021-12-16 00:24:37 +0100

    day 5 initial commit

--- all-days.zig
+++ all-days.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
+
 const day_1 = @import("day-1.zig");
 const day_2 = @import("day-2.zig");
 const day_3 = @import("day-3.zig");
 const day_4 = @import("day-4.zig");
+const day_5 = @import("day-5.zig");
 
 pub fn main() !void {
     try day_1.main();
@@ -12,4 +14,6 @@ pub fn main() !void {
     try day_3.main();
     std.debug.print("\n", .{});
     try day_4.main();
+    std.debug.print("\n", .{});
+    try day_5.main();
 }
---
+++ day-5.zig
@@ -0,0 +1,8 @@
+const std = @import("std");
+
+const use_test_input = true;
+const filename = if (use_test_input) "day-5_test-input" else "day-5_real-input";
+
+pub fn main() !void {
+    std.debug.print("--- Day 5 ---\n", .{});
+}
