c5fd74446f0e3fd98c1f6c8a368e686d7265c7b9
GigaGrunch <gigagrunch@gmail.com>
2021-12-16 19:45:43 +0100

    gather initial fish counts

--- day-6.zig
+++ day-6.zig
@@ -1,5 +1,26 @@
 const std = @import("std");
 
+const use_test_input = true;
+const filename = if (use_test_input) "day-6_test-input" else "day-6_real-input";
+
 pub fn main() !void {
     std.debug.print("--- Day 6 ---\n", .{});
+
+    var fish_count = [_]u32 {0} ** 9;
+
+    var file = try std.fs.cwd().openFile(filename, .{});
+    while (true) {
+        const char = try file.reader().readByte();
+        if (char == ',') continue;
+        if (char == '\n') break;
+
+        const timer = @intCast(usize, char - '0');
+        fish_count[timer] += 1;
+    }
+
+    std.debug.print("initial fish counts: ", .{});
+    for (fish_count) |count, i| {
+        std.debug.print("{}x{} ", .{ count, i });
+    }
+    std.debug.print("\n", .{});
 }
