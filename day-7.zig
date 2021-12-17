b286dbbdd5eaee2b1e4066c2a1d9a988635b161c
GigaGrunch <gigagrunch@gmail.com>
2021-12-18 00:09:54 +0100

    implement quick sort

--- day-7.zig
+++ day-7.zig
@@ -1,7 +1,5 @@
 const std = @import("std");
 
-var i: usize = undefined;
-
 const use_test_input = true;
 const filename = if (use_test_input) "day-7_test-input" else "day-7_real-input";
 const input_length = if (use_test_input) 10 else 1000;
@@ -12,7 +10,7 @@ pub fn main() !void {
     var file = try std.fs.cwd().openFile(filename, .{});
 
     var crab_positions: [input_length]u16 = undefined;
-    i = 0;
+    var i: usize = 0;
     while (i < input_length):(i += 1) {
         var buffer: [4]u8 = undefined;
         const delimiter: u8 = if (i != input_length - 1) ',' else '\n';
@@ -20,9 +18,56 @@ pub fn main() !void {
         crab_positions[i] = try std.fmt.parseInt(u16, pos_string, 10);
     }
 
-    std.debug.print("positions: ", .{});
-    for (crab_positions) |pos| {
-        std.debug.print("{} ", .{ pos });
+    // quickSort(crab_positions[0..]);
+}
+
+fn quickSort(array: []u16) void {
+    var pivot = array.len - 1;
+
+    var greater: usize = 0;
+    var smaller: usize = 0;
+
+    while (true) {
+        while (greater < pivot):(greater += 1) {
+            if (array[greater] > array[pivot]) {
+                break;
+            }
+        }
+
+        if (smaller < greater) {
+            smaller = greater;
+        }
+
+        while (smaller < pivot):(smaller += 1) {
+            if (array[smaller] < array[pivot]) {
+                break;
+            }
+        }
+
+        if (greater == pivot or smaller == pivot) {
+            break;
+        }
+
+        const tmp = array[greater];
+        array[greater] = array[smaller];
+        array[smaller] = tmp;
     }
-    std.debug.print("\n", .{});
+
+    {
+        const tmp = array[greater];
+        array[greater] = array[pivot];
+        array[pivot] = tmp;
+    }
+
+    if (greater > 0) {
+        quickSort(array[0..greater]);
+    }
+    if (greater < array.len - 1) {
+        quickSort(array[(greater + 1)..]);
+    }
+}
+
+test "sort" {
+    var array = [_]u16 { 8, 7, 6, 1, 0, 9, 2 };
+    quickSort(array[0..]);
 }
