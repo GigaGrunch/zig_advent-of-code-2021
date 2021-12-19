6725bab7fc138d421bbe5eaceaa3328bf8dd2698
GigaGrunch <gigagrunch@gmail.com>
2021-12-19 01:37:41 +0100

    add day 7 to all-days program

--- all-days.zig
+++ all-days.zig
@@ -6,6 +6,7 @@ const day_3 = @import("day-3.zig");
 const day_4 = @import("day-4.zig");
 const day_5 = @import("day-5.zig");
 const day_6 = @import("day-6.zig");
+const day_7 = @import("day-7.zig");
 
 pub fn main() !void {
     try day_1.main();
@@ -19,4 +20,6 @@ pub fn main() !void {
     try day_5.main();
     std.debug.print("\n", .{});
     try day_6.main();
+    std.debug.print("\n", .{});
+    try day_7.main();
 }
