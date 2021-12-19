f72f6697c2b7053b5d2c1623c425a6deb0ada6d5
GigaGrunch <gigagrunch@gmail.com>
2021-12-20 00:07:19 +0100

    calculate basin sizes

--- day-9.zig
+++ day-9.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const use_test_input = false;
+const use_test_input = true;
 const filename = if (use_test_input) "day-9_test-input" else "day-9_real-input";
 const line_length = if (use_test_input) 10 else 100;
 const line_count = if (use_test_input) 5 else 100;
@@ -11,52 +11,115 @@ pub fn main() !void {
     var file = try std.fs.cwd().openFile(filename, .{});
     defer file.close();
 
-    var buffers: [3][line_length + 1]u8 = undefined;
-    var next_buffer_index: usize = 0;
+    var buffer: [1024 * 1024]u8 = undefined;
 
-    var prev_line: []const u8 = undefined;
-    var line: []const u8 = undefined;
-    var next_line: []const u8 = undefined;
-
-    next_line = try file.reader().readUntilDelimiter(buffers[next_buffer_index][0..], '\n');
-    next_buffer_index = (next_buffer_index + 1) % buffers.len;
-
-    var low_points_sum: u32 = 0;
-
-    var line_index: u8 = 0;
-    while (line_index < line_count):(line_index += 1) {
-        if (line_index > 0) {
-            prev_line = line;
-        }
-
-        line = next_line;
-
-        if (line_index < line_count - 1) {
-            next_line = try file.reader().readUntilDelimiter(buffers[next_buffer_index][0..], '\n');
-            next_buffer_index = (next_buffer_index + 1) % buffers.len;
-        }
-
-        for (line) |char, char_index| {
-            if (char_index > 0) {
-                if (line[char_index - 1] <= char) continue;
+    var map: [line_length * line_count]u4 = undefined;
+    {
+        var line_i: usize = 0;
+        while (line_i < line_count):(line_i += 1) {
+            const line = try file.reader().readUntilDelimiter(buffer[0..], '\n');
+            for (line) |char, char_i| {
+                const map_i = line_i * line_length + char_i;
+                map[map_i] = parseInt(char);
             }
-            if (char_index < line_length - 1) {
-                if (line[char_index + 1] <= char) continue;
-            }
-            if (line_index > 0) {
-                if (prev_line[char_index] <= char) continue;
-            }
-            if (line_index < line_count - 1) {
-                if (next_line[char_index] <= char) continue;
-            }
-
-            low_points_sum += parseInt(char) + 1;
         }
     }
 
-    std.debug.print("sum of all low points is {}\n", .{ low_points_sum });
+    var buffer_allocator = std.heap.FixedBufferAllocator.init(buffer[0..]);
+
+    var low_points = std.ArrayList(usize).init(buffer_allocator.allocator());
+    {
+        var y: usize = 0;
+        while (y < line_count):(y += 1) {
+            var x: usize = 0;
+            while (x < line_length):(x += 1) {
+                if (x > 0) {
+                    if (map[getIndex(x - 1, y)] <= map[getIndex(x, y)]) continue;
+                }
+                if (x < line_length - 1) {
+                    if (map[getIndex(x + 1, y)] <= map[getIndex(x, y)]) continue;
+                }
+                if (y > 0) {
+                    if (map[getIndex(x, y - 1)] <= map[getIndex(x, y)]) continue;
+                }
+                if (y < line_count - 1) {
+                    if (map[getIndex(x, y + 1)] <= map[getIndex(x, y)]) continue;
+                }
+
+                try low_points.append(getIndex(x, y));
+            }
+        }
+    }
+
+    // var basin_sizes = [_]u32 {0} ** 3;
+    {
+        for (low_points.items) |low_point_index| {
+            var size: u32 = 1;
+
+            var visited = std.ArrayList(usize).init(buffer_allocator.allocator());
+            var frontier = std.ArrayList(usize).init(buffer_allocator.allocator());
+
+            const low_point_coords = getCoords(low_point_index);
+            if (low_point_coords.x > 0) {
+                try frontier.append(low_point_index - 1);
+            }
+            if (low_point_coords.x < line_length - 1) {
+                try frontier.append(low_point_index + 1);
+            }
+            if (low_point_coords.y > 0) {
+                try frontier.append(low_point_index - line_length);
+            }
+            if (low_point_coords.y < line_count - 1) {
+                try frontier.append(low_point_index + line_length);
+            }
+
+            while (frontier.items.len != 0) {
+                const index = frontier.pop();
+                if (index == low_point_index) continue;
+                if (map[index] == 9) continue;
+                if (contains(visited.items, index)) continue;
+                try visited.append(index);
+
+                size += 1;
+
+                const coords = getCoords(index);
+                if (coords.x > 0) {
+                    try frontier.append(index - 1);
+                }
+                if (coords.x < line_length - 1) {
+                    try frontier.append(index + 1);
+                }
+                if (coords.y > 0) {
+                    try frontier.append(index - line_length);
+                }
+                if (coords.y < line_count - 1) {
+                    try frontier.append(index + line_length);
+                }
+            }
+
+            std.debug.print("basin size: {}\n", .{ size });
+        }
+    }
 }
 
-fn parseInt(char: u8) u8 {
-    return char - '0';
+fn contains(haystack: []usize, needle: usize) bool {
+    for (haystack) |item| {
+        if (item == needle) return true;
+    }
+    return false;
+}
+
+fn getCoords(index: usize) struct { x: usize, y: usize } {
+    return .{
+        .x = index % line_length,
+        .y = index / line_length,
+    };
+}
+
+fn getIndex(x: usize, y: usize) usize {
+    return y * line_length + x;
+}
+
+fn parseInt(char: u8) u4 {
+    return @intCast(u4, char - '0');
 }
