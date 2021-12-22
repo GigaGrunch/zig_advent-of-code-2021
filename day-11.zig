22c8d0c4b9d0ec369afbf1054ca7d68d3540796f
Benjamin Schmithüsen <benjamin.schmithuesen@ravensburger.de>
2021-12-22 11:15:49 +0100

    determine when all octopuses flash together the first time

--- day-11.zig
+++ day-11.zig
@@ -23,10 +23,8 @@ pub fn main() !void {
         }
     }
 
-    var flash_count: u32 = 0;
-
-    var iteration: u32 = 0;
-    while (iteration < 100):(iteration += 1) {
+    var iteration: u32 = 1;
+    while (iteration <= 1000):(iteration += 1) {
         var flash_buffer: [3 * edge_length * edge_length * @sizeOf(usize)]u8 = undefined;
         var flash_allocator = std.heap.FixedBufferAllocator.init(flash_buffer[0..]);
         var flash_indices = try std.ArrayList(usize).initCapacity(flash_allocator.allocator(), edge_length * edge_length);
@@ -46,7 +44,6 @@ pub fn main() !void {
 
             grid[flash_index] = 200;
             try handles_indices.append(flash_index);
-            flash_count += 1;
 
             var coords = getCoords(flash_index);
             for ([_]i8 { -1, 0, 1 }) |y_offset| {
@@ -71,12 +68,15 @@ pub fn main() !void {
             }
         }
 
+        if (handles_indices.items.len == edge_length * edge_length) {
+            std.debug.print("all octopuses flashed in iteration {}\n", .{ iteration });
+            break;
+        }
+
         for (handles_indices.items) |i| {
             grid[i] = 0;
         }
     }
-
-    std.debug.print("total flash count is {} after {} iterations\n", .{ flash_count, iteration });
 }
 
 fn printGrid(grid: []u8) void {
