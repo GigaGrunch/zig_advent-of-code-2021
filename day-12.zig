5170f020f907b76388a631534e593ee15213dcf7
Benjamin Schmithüsen <benjamin.schmithuesen@ravensburger.de>
2021-12-23 12:03:24 +0100

    pass all tests according to new rules

--- day-12.zig
+++ day-12.zig
@@ -53,7 +53,10 @@ fn execute(input: []const u8) !u32 {
 
     var initial_path = std.ArrayList(Cave).init(alloc.allocator());
     try initial_path.append("start");
-    const path_count = try continuePath(initial_path, connections.items);
+    const path_count = try continuePath(.{
+        .path = initial_path,
+        .connections = connections.items,
+    });
 
     return path_count;
 }
@@ -71,26 +74,42 @@ fn getCaveType(cave: []const u8) CaveType {
     unreachable;
 }
 
-fn continuePath(path: std.ArrayList(Cave), connections: []Connection) PathError!u32 {
-    const pos = path.items[path.items.len - 1];
+fn continuePath(in: struct { path: std.ArrayList(Cave), double_small_cave_chosen: bool = false, connections: []Connection }) PathError!u32 {
+    const pos = in.path.items[in.path.items.len - 1];
 
     if (getCaveType(pos) == .End) return 1;
 
     var sub_paths: u32 = 0;
 
-    for (connections) |connection| {
+    for (in.connections) |connection| {
         if (!equals(connection.from, pos)) continue;
+
+        var is_small = false;
+        var num_visits: u32 = 0;
 
         switch (getCaveType(connection.to)) {
             .Start => continue,
-            .Small => if (contains(path.items, connection.to)) continue,
+            .Small => {
+                is_small = true;
+                num_visits = count(in.path.items, connection.to);
+                switch (num_visits) {
+                    0 => { },
+                    1 => if (in.double_small_cave_chosen) continue,
+                    2 => continue,
+                    else => unreachable
+                }
+            },
             else => { }
         }
 
-        var sub_path = try std.ArrayList(Cave).initCapacity(path.allocator, path.items.len + 1);
-        try sub_path.appendSlice(path.items);
+        var sub_path = try std.ArrayList(Cave).initCapacity(in.path.allocator, in.path.items.len + 1);
+        try sub_path.appendSlice(in.path.items);
         try sub_path.append(connection.to);
-        sub_paths += try continuePath(sub_path, connections);
+        sub_paths += try continuePath(.{ 
+            .path = sub_path,
+            .double_small_cave_chosen = in.double_small_cave_chosen or (is_small and num_visits == 1),
+            .connections = in.connections
+        });
         sub_path.deinit();
     }
 
@@ -103,6 +122,14 @@ fn contains(haystack: []Cave, needle: Cave) bool {
     return for (haystack) |cave| {
         if (equals(cave, needle)) break true;
     } else false;
+}
+
+fn count(haystack: []Cave, needle: Cave) u32 {
+    var result: u32 = 0;
+    for (haystack) |cave| {
+        if (equals(cave, needle)) result += 1;
+    }
+    return result;
 }
 
 fn equals(lhs: Cave, rhs: Cave) bool {
@@ -125,18 +152,18 @@ const Connection = struct {
 
 test "test-input-1" {
     const result = try execute(test_input_1);
-    const expected: u32 = 10;
+    const expected: u32 = 36;
     try std.testing.expectEqual(expected, result);
 }
 
 test "test-input-2" {
     const result = try execute(test_input_2);
-    const expected: u32 = 19;
+    const expected: u32 = 103;
     try std.testing.expectEqual(expected, result);
 }
 
 test "test-input-3" {
     const result = try execute(test_input_3);
-    const expected: u32 = 226;
+    const expected: u32 = 3509;
     try std.testing.expectEqual(expected, result);
 }
