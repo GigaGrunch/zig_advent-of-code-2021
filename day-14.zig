8c04671d897bb6c56fdc27c7995784a56538d4db
GigaGrunch <gigagrunch@gmail.com>
2021-12-26 13:42:32 +0100

    parse rules

--- day-14.zig
+++ day-14.zig
@@ -17,11 +17,39 @@ test "test-input" {
 }
 
 fn execute(input: []const u8) !u32 {
-    var line_it = std.mem.tokenize(u8, input, "\r\n");
-    
-    const template = line_it.next() orelse unreachable;
+    var buffer: [1024 * 1024]u8 = undefined;
+    var alloc = std.heap.FixedBufferAllocator.init(buffer[0..]);
 
+    var line_it = std.mem.tokenize(u8, input, "\r\n");
+    const template = line_it.next() orelse unreachable;
     std.debug.print("template: {s}\n", .{ template });
+
+    var rules = std.ArrayList(Rule).init(alloc.allocator());
+
+    while (line_it.next()) |line| {
+        if (line.len == 0) continue;
+
+        var rule_it = std.mem.tokenize(u8, line, " ->");
+        const inPair = rule_it.next() orelse unreachable;
+        const outChar = rule_it.next() orelse unreachable;
+
+        try rules.append(.{
+            .inPair = inPair,
+            .outChar = outChar[0],
+        });
+    }
+
+    for (rules.items) |rule| {
+        std.debug.print("{s} -> {c}\n", .{ rule.inPair, rule.outChar });
+    }
+
+    // var string = std.ArrayList(u8).init(alloc.allocator());
+    // try string.appendSlice(template);
 
     return @intCast(u32, input.len);
 }
+
+const Rule = struct {
+    inPair: []const u8,
+    outChar: u8,
+};
