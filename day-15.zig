d423873711554eba2d3fa2ae1030e546ef7bc714
GigaGrunch <gigagrunch@gmail.com>
2022-01-01 21:15:22 +0100

    calculate lowest cost of each position (only 1 iteration)

--- day-15.zig
+++ day-15.zig
@@ -12,12 +12,16 @@ fn execute(input: []const u8) !u32 {
     var alloc_buffer: [1024 * 1024]u8 = undefined;
     var alloc = std.heap.FixedBufferAllocator.init(alloc_buffer[0..]);
 
-    var map = std.ArrayList(Pos).init(alloc.allocator());
-    defer map.deinit();
+    var map_list = std.ArrayList(Pos).init(alloc.allocator());
+    defer map_list.deinit();
+
+    var edge_length: usize = undefined;
 
     var input_it = std.mem.tokenize(u8, input, "\r\n");
     while (input_it.next()) |line| {
         if (line.len == 0) continue;
+
+        edge_length = line.len;
 
         for (line) |pos_cost_char| {
             const cost = pos_cost_char - '0';
@@ -25,15 +29,46 @@ fn execute(input: []const u8) !u32 {
                 .individual_cost = cost,
             };
 
-            try map.append(pos);
+            try map_list.append(pos);
         }
     }
 
-    return 0;
+    var map = map_list.items;
+    map[0].lowest_cost = 0;
+
+    for (map) |pos, i| {
+        const x = i % edge_length;
+        const y = i / edge_length;
+
+        var neighbor_indices = std.ArrayList(usize).init(alloc.allocator());
+        defer neighbor_indices.deinit();
+
+        if (x > 0) {
+            try neighbor_indices.append(i - 1);
+        }
+        if (x < edge_length - 1) {
+            try neighbor_indices.append(i + 1);
+        }
+        if (y > 0) {
+            try neighbor_indices.append(i - edge_length);
+        }
+        if (y < edge_length - 1) {
+            try neighbor_indices.append(i + edge_length);
+        }
+
+        for (neighbor_indices.items) |neighbor_i| {
+            const neighbor = &map[neighbor_i];
+            const potential_cost = pos.lowest_cost + neighbor.individual_cost;
+            neighbor.lowest_cost = @minimum(potential_cost, neighbor.lowest_cost);
+        }
+    }
+
+    return map[map.len - 1].lowest_cost;
 }
 
 const Pos = struct {
     individual_cost: u32,
+    lowest_cost: u32 = std.math.maxInt(u32),
 };
 
 test "test-input" {
