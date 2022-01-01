0fc0c7b96024c20af891aa119595ee3a568c41d7
GigaGrunch <gigagrunch@gmail.com>
2022-01-01 21:01:57 +0100

    parse input

--- day-15.zig
+++ day-15.zig
@@ -9,9 +9,32 @@ pub fn main() !void {
 }
 
 fn execute(input: []const u8) !u32 {
-    assert(input.len > 0, "input is empty");
+    var alloc_buffer: [1024 * 1024]u8 = undefined;
+    var alloc = std.heap.FixedBufferAllocator.init(alloc_buffer[0..]);
+
+    var map = std.ArrayList(Pos).init(alloc.allocator());
+    defer map.deinit();
+
+    var input_it = std.mem.tokenize(u8, input, "\r\n");
+    while (input_it.next()) |line| {
+        if (line.len == 0) continue;
+
+        for (line) |pos_cost_char| {
+            const cost = pos_cost_char - '0';
+            const pos = Pos {
+                .individual_cost = cost,
+            };
+
+            try map.append(pos);
+        }
+    }
+
     return 0;
 }
+
+const Pos = struct {
+    individual_cost: u32,
+};
 
 test "test-input" {
     const expected: u32 = 40;
