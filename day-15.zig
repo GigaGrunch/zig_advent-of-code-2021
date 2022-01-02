6a50fe7ca02fb37ed0345959060ae82b86385f2d
GigaGrunch <gigagrunch@gmail.com>
2022-01-02 09:26:39 +0100

    unfold input map

--- day-15.zig
+++ day-15.zig
@@ -12,16 +12,16 @@ fn execute(input: []const u8, unfold_map: bool) !u32 {
     var alloc_buffer: [1024 * 1024]u8 = undefined;
     var alloc = std.heap.FixedBufferAllocator.init(alloc_buffer[0..]);
 
-    var map_list = std.ArrayList(Pos).init(alloc.allocator());
-    defer map_list.deinit();
+    var initial_map = std.ArrayList(Pos).init(alloc.allocator());
+    defer initial_map.deinit();
 
-    var edge_length: usize = undefined;
+    var initial_edge_length: usize = undefined;
 
     var input_it = std.mem.tokenize(u8, input, "\r\n");
     while (input_it.next()) |line| {
         if (line.len == 0) continue;
 
-        edge_length = line.len;
+        initial_edge_length = line.len;
 
         for (line) |pos_cost_char| {
             const cost = pos_cost_char - '0';
@@ -29,15 +29,39 @@ fn execute(input: []const u8, unfold_map: bool) !u32 {
                 .individual_cost = cost,
             };
 
-            try map_list.append(pos);
+            try initial_map.append(pos);
         }
     }
 
+    var map: []Pos = undefined;
+    var edge_length: usize = undefined;
+
     if (unfold_map) {
-        // TODO: unfold map (day 2)
+        var full_map = std.ArrayList(Pos).init(alloc.allocator());
+        edge_length = initial_edge_length * 5;
+
+        var y: usize = 0;
+        while (y < edge_length):(y += 1) {
+            var x: usize = 0;
+            while (x < edge_length):(x += 1) {
+                const initial_map_x = x % initial_edge_length;
+                const initial_map_y = y % initial_edge_length;
+                const initial_map_i = initial_map_y * initial_edge_length + initial_map_x;
+                const increment = (x / initial_edge_length) + (y / initial_edge_length);
+                var pos = initial_map.items[initial_map_i];
+                pos.individual_cost += @intCast(u32, increment);
+                while (pos.individual_cost > 9) pos.individual_cost -= 9;
+                try full_map.append(pos);
+            }
+        }
+
+        map = full_map.items;
+    }
+    else {
+        map = initial_map.items;
+        edge_length = initial_edge_length;
     }
 
-    var map = map_list.items;
     map[0].lowest_cost = 0;
 
     var anything_changed = true;
