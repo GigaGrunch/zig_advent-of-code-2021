a0b44097a81d4a5e9b22113bb5311a75a3d7bcdc
Benjamin Schmithüsen <benjamin.schmithuesen@ravensburger.de>
2022-01-05 15:26:08 +0100

    simulate probe flight

--- day-17.zig
+++ day-17.zig
@@ -4,6 +4,49 @@ pub fn main() !void {
     std.debug.print("--- Day 17 ---\n", .{});
 }
 
+fn simulateProbe(target: Rect, initial_velocity: Vector) struct{ highest_y: i32, is_hit: bool, } {
+    var probe = Probe { .vel = initial_velocity };
+    var highest_y: i32 = probe.pos.y;
+
+    var is_hit = while (probe.pos.x <= target.max_x and probe.pos.y >= target.min_y):(probe.step()) {
+        highest_y = @maximum(highest_y, probe.pos.y);
+
+        if (probe.pos.x >= target.min_x and
+            probe.pos.y <= target.max_y) {
+            break true;
+        }
+    } else false;
+
+    return .{ .highest_y = highest_y, .is_hit = is_hit };
+}
+
+test "simulateProbe" {
+    const input_area = Rect { .min_x = 20, .max_x = 30, .min_y = -10, .max_y = -5 };
+    const initial_velocity = Vector { .x = 6, .y = 9 };
+    const expected_hit = true;
+    const expected_y: i32 = 45;
+    const result = simulateProbe(input_area, initial_velocity);
+    try std.testing.expectEqual(expected_hit, result.is_hit);
+    try std.testing.expectEqual(expected_y, result.highest_y);
+}
+
+const Probe = struct {
+    pos: Vector = .{ .x = 0, .y = 0 },
+    vel: Vector,
+
+    fn step(probe: *@This()) void {
+        probe.pos.x += probe.vel.x;
+        probe.pos.y += probe.vel.y;
+        if (probe.vel.x > 0) probe.vel.x -= 1;
+        probe.vel.y -= 1;
+    }
+};
+
+const Vector = struct {
+    x: i32,
+    y: i32,
+};
+
 const Rect = struct {
     min_x: i32,
     max_x: i32,
