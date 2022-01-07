2fe200142eeab99eac33e6c4e53afb13f7b90ed9
GigaGrunch <gigagrunch@gmail.com>
2022-01-07 18:34:55 +0100

    parse beacons

--- day-19.zig
+++ day-19.zig
@@ -12,10 +12,9 @@ test "test-input" {
     defer alloc.deinit();
 
     // var scanners = std.ArrayList(Scanner).init(alloc.allocator());
-    // var beacons = std.ArrayList(Pos).init(alloc.allocator());
+    var beacons = std.ArrayList(Pos).init(alloc.allocator());
 
     var scanner_count: u32 = 0;
-    var beacon_count: u32 = 0;
 
     var line_it = std.mem.tokenize(u8, input, "\n\r");
     while (line_it.next()) |line| {
@@ -23,14 +22,34 @@ test "test-input" {
 
         if (std.mem.startsWith(u8, line, "--- scanner ")) {
             scanner_count += 1;
-            continue;
         }
-
-        beacon_count += 1;
+        else {
+            const beacon = try parseBeacon(line);
+            try beacons.append(beacon);
+        }
     }
 
     try std.testing.expectEqual(@as(u32, 5), scanner_count);
-    try std.testing.expectEqual(@as(u32, 127), beacon_count);
+    try std.testing.expectEqual(@as(usize, 127), beacons.items.len);
+}
+
+fn parseBeacon(line: []const u8) !Pos {
+    var coord_it = std.mem.tokenize(u8, line, ",");
+    return Pos {
+        .x = try std.fmt.parseInt(i32, coord_it.next().?, 10),
+        .y = try std.fmt.parseInt(i32, coord_it.next().?, 10),
+        .z = try std.fmt.parseInt(i32, coord_it.next().?, 10),
+    };
+}
+
+test "parseBeacon" {
+    const input = "-456,654,0";
+    const expected = Pos {
+        .x = -456,
+        .y = 654,
+        .z = 0,
+    };
+    try std.testing.expectEqual(expected, try parseBeacon(input));
 }
 
 const Scanner = struct {
