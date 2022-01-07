d27e59d21592a94f2cdd1f67a48b9332e0144fdf
GigaGrunch <gigagrunch@gmail.com>
2022-01-07 18:28:56 +0100

    detect scanner and beacon lines

--- day-19.zig
+++ day-19.zig
@@ -15,15 +15,22 @@ test "test-input" {
     // var beacons = std.ArrayList(Pos).init(alloc.allocator());
 
     var scanner_count: u32 = 0;
+    var beacon_count: u32 = 0;
 
     var line_it = std.mem.tokenize(u8, input, "\n\r");
     while (line_it.next()) |line| {
+        if (line.len == 0) continue;
+
         if (std.mem.startsWith(u8, line, "--- scanner ")) {
             scanner_count += 1;
+            continue;
         }
+
+        beacon_count += 1;
     }
 
     try std.testing.expectEqual(@as(u32, 5), scanner_count);
+    try std.testing.expectEqual(@as(u32, 127), beacon_count);
 }
 
 const Scanner = struct {
